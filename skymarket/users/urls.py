a58b30c19fd751305f3c665786876567d4ef90ac
stanislav <stanislav.vasilchenko@yandex.ru>
2024-04-04 23:07:20 +0300

    add register url

--- skymarket/users/urls.py
+++ skymarket/users/urls.py
@@ -1,8 +1,17 @@
-from django.urls import include, path
-from djoser.views import UserViewSet
-from rest_framework.routers import SimpleRouter
+from django.urls import path
+
+from users.views import UserRegistrationAPIView
+
+# from djoser.views import UserViewSet
+# from rest_framework.routers import SimpleRouter
+
+
+
 # TODO подключите UserViewSet из Djoser.views к нашим urls.py
 # TODO для этокого рекоммендуется использовать SimpleRouter
 
+app_name = 'users'
+
 urlpatterns = [
+    path('register/', UserRegistrationAPIView.as_view(), name='registration')
 ]
