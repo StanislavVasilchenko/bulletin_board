71e172c35db470d8e62098f255ae715522808612
stanislav <stanislav.vasilchenko@yandex.ru>
2024-04-04 23:06:51 +0300

    create UserRegistrationSerializer

--- skymarket/users/serializers.py
+++ skymarket/users/serializers.py
@@ -1,4 +1,4 @@
-from djoser.serializers import UserCreateSerializer as BaseUserRegistrationSerializer
+# from djoser.serializers import UserCreateSerializer as BaseUserRegistrationSerializer
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
@@ -7,8 +7,10 @@ User = get_user_model()
 # TODO для создания пользователя из за того, что у нас имеются нестандартные поля
 
 
-class UserRegistrationSerializer(BaseUserRegistrationSerializer):
-    pass
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'phone')
 
 
 class CurrentUserSerializer(serializers.ModelSerializer):
