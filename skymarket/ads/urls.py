8a7369bb78123ea0fa3b1979084a40fd9c2c0b87
stanislav <stanislav.vasilchenko@yandex.ru>
2024-04-08 17:21:02 +0300

    add ads/me/ in urls

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path
 from rest_framework import routers
 
 from ads import views
 from ads.apps import SalesConfig
+
+from ads.views import AdViewSet
 
 # TODO настройка роутов для модели
 
@@ -10,8 +13,7 @@ app_name = SalesConfig.name
 ad_router = routers.DefaultRouter()
 ad_router.register('ads', views.AdViewSet, basename='ads')
 
-
 urlpatterns = [
-
+    path('ads/me/', AdViewSet.as_view({'get': 'get_me_ads'}), name='me_ads'),
 ]
 urlpatterns += ad_router.urls
