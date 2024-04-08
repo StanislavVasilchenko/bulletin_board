bc24be90ce7b17ffffdf700e95e294f82bbd65fe
stanislav <stanislav.vasilchenko@yandex.ru>
2024-04-08 17:21:55 +0300

    create IsAdsOwner, IsAdmin perm

--- skymarket/ads/permissions.py
+++ skymarket/ads/permissions.py
@@ -1,1 +1,12 @@
 # TODO здесь производится настройка пермишенов для нашего проекта
+from rest_framework import permissions
+
+
+class IsAdsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role == 'admin'
