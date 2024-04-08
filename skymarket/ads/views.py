5f3bb6cd30e350a95a6beec1be6b4f0adb9b2e83
stanislav <stanislav.vasilchenko@yandex.ru>
2024-04-08 17:20:40 +0300

    add perform_create, get_permissions, get_me_ads in AdViewSet

--- skymarket/ads/views.py
+++ skymarket/ads/views.py
@@ -1,6 +1,11 @@
-from rest_framework import pagination, viewsets
+from rest_framework import pagination, viewsets, status
 from ads.models import Ad
 from ads.serializers import AdSerializer
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+from ads.permissions import IsAdsOwner, IsAdmin
+from rest_framework.response import Response
 
 
 class AdPagination(pagination.PageNumberPagination):
@@ -13,6 +18,25 @@ class AdViewSet(viewsets.ModelViewSet):
     queryset = Ad.objects.all()
     serializer_class = AdSerializer
     pagination_class = AdPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'create', 'destroy']:
+            self.permission_classes = [IsAuthenticated, IsAdsOwner | IsAdmin]
+        elif self.action in ['list', 'retrieve']:
+            self.permission_classes = [AllowAny]
+        return [permission() for permission in self.permission_classes]
+
+    @action(detail=False, methods=['get'])
+    def get_me_ads(self, request):
+        if request.user.is_authenticated:
+            queryset = Ad.objects.filter(author=self.request.user).order_by('-id')
+            serializer = AdSerializer(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response([{'message': 'Not logged in'}], status=status.HTTP_401_UNAUTHORIZED)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
