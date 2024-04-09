db7ec33289d96dca6c96e041b1e68a1f5c6193d8
stanislav <stanislav.vasilchenko@yandex.ru>
2024-04-09 16:35:29 +0300

    add get-ad-comments url

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -14,10 +14,11 @@ ad_router = routers.DefaultRouter()
 ad_router.register('ads', views.AdViewSet, basename='ads')
 
 comment_router = routers.DefaultRouter()
-comment_router.register('comment', viewset=views.CommentViewSet, basename='comment')
+comment_router.register('comments', viewset=views.CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('ads/me/', AdViewSet.as_view({'get': 'get_me_ads'}), name='me_ads'),
     path('ads/<int:ad_id>/', include(comment_router.urls), name='create-comment'),
+    path('api/ads/<int:ad_pk>/', include(comment_router.urls), name='get-ad-comments'),
 ]
 urlpatterns += ad_router.urls
