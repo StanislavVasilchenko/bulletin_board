42ac9ef35693c9eccdd33e6fdd15450157973590
stanislav <stanislav.vasilchenko@yandex.ru>
2024-04-09 15:37:21 +0300

    add comments urls

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 
 from ads import views
 from ads.apps import SalesConfig
 
-from ads.views import AdViewSet
+from ads.views import AdViewSet, CommentViewSet
 
 # TODO настройка роутов для модели
 
@@ -13,7 +13,11 @@ app_name = SalesConfig.name
 ad_router = routers.DefaultRouter()
 ad_router.register('ads', views.AdViewSet, basename='ads')
 
+comment_router = routers.DefaultRouter()
+comment_router.register('comment', viewset=views.CommentViewSet, basename='comment')
+
 urlpatterns = [
     path('ads/me/', AdViewSet.as_view({'get': 'get_me_ads'}), name='me_ads'),
+    path('ads/<int:ad_id>/', include(comment_router.urls), name='create-comment'),
 ]
 urlpatterns += ad_router.urls
