3ff2d5f691cda535af2b848739829334167b5bf9
stanislav <stanislav.vasilchenko@yandex.ru>
2024-04-09 16:35:04 +0300

    rename IsAdsOwner -> IsOwner

--- skymarket/ads/permissions.py
+++ skymarket/ads/permissions.py
@@ -2,7 +2,7 @@
 from rest_framework import permissions
 
 
-class IsAdsOwner(permissions.BasePermission):
+class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.author == request.user
 
